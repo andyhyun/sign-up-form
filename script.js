834c491bbdd0c8d8e5f602a1e7df84e51f6c6665
andyhyun <andyhyukim@gmail.com>
2023-07-28 17:47:15 -0400

    feat: add check for matching passwords

--- script.js
+++ script.js
@@ -2,4 +2,24 @@
 
 const password = document.querySelector("#password");
 const confirm = document.querySelector("#confirm");
+const errorMessage = document.querySelector("span.error-message");
+
+function checkPasswordMatch(event) {
+  if (password.value == confirm.value) {
+    password.className = "error";
+    confirm.className = "error";
+    errorMessage.textContent = "";
+  } else {
+    showError();
+  }
+}
+
+function showError() {
+  password.className = "error active";
+  confirm.className = "error active";
+  errorMessage.textContent = "* Passwords do not match";
+}
+
+password.addEventListener("input", checkPasswordMatch);
+confirm.addEventListener("input", checkPasswordMatch);
 
